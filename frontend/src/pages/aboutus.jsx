2d016e9d78207e575ea8d8f7eddd8b3bcb86a805
FatemaHaque <haque.marwa@gmail.com>
2023-12-14 18:31:28 -0800

    first project update

---
+++ frontend/src/pages/aboutus.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function Aboutus() {
+  return (
+    <>
+    <div className='bg-gray-200 w-full h-full px-20 py-12'>
+      <h2 className=''>About Us</h2>
+
+      <p>Welcome to TURAMY – your digital bridge to the world's most breathtaking destinations. Our mission is simple: to enrich lives through travel by making it more accessible, convenient, and unforgettable. Whether you're planning a weekend getaway or a grand expedition, URAMY is the travel companion you've been waiting for.</p>
+
+      <h2>Intuitive Booking, Unforgettable Journeys</h2>
+
+<p>Navigating TURAMY is a breeze. Create your account, log in, and embark on a journey of easy planning and booking. Our platform is designed to inspire and facilitate travelers of all types, with tools and services that make organizing your next trip as enjoyable as the trip itself.</p>
+
+<h2>Your Stay, Your Way</h2>
+
+<p></p>Discover the perfect accommodation for every adventure with TURAMY. We offer a diverse range of hotels to suit every traveler's needs and budgets. From boutique to luxury, find and book your ideal stay with real-time availability and secure, hassle-free booking.
+
+<h2>Tours Tailored for You</h2>
+
+<p>URAMY connects you with the most enriching and exhilarating tours available. Local expertise guides our handpicked experiences, ensuring you dive deep into the essence of each destination. Culture, adventure, relaxation – whatever your travel heart desires, we have a tour to match.</p>
+
+<h2>Local Stays at a Glance</h2>
+
+<p>Our innovative platform allows you to find the nearest hotels at your destination with just a quick search. URAMY makes sure you're never without a comfortable and convenient place to rest, no matter where your travels take you.</p>
+
+<h2>Your Journey Starts Here</h2>
+
+<p>At TURAMY, we're passionate about travel and committed to providing the tools and support you need to explore the world with confidence and ease. We're not just a service; we're a community of wanderers and explorers, just like you. So why wait? Sign up, log in, and let URAMY take you somewhere extraordinary.</p>
+    </div>
+    </>
+  )
+}
